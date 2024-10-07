8855265993a331aecacafa0b707392c52a048f9b
Abel-D <abeldebebe818@gmail.com>
2024-10-07 17:37:55 +0300

    removed vercel-plugin and iframe controls fixed

--- src/components/landing-header/landing-header.jsx
+++ src/components/landing-header/landing-header.jsx
@@ -32,9 +32,14 @@ function LandingHeader({content}){
                         <h2 className='firsttxt grower text-primary font-bold'>Hi there, </h2>
                         <div className=" text-2xl text-primary font-bold "><h2>I am <span className='txt text-6xl font-bold my-3'>Abel Debebe</span></h2></div>
                         <h2 className="thirdtxt mt-0 mb-4 text-300 text-xl line-height-3">Full-stack Engineer - React, Angular, Next, .Net, Python, Django</h2>
-                     { blobs &&
+                     {/* { blobs &&
                         <video className="border-round-3xl h-full w-full lg:ml-6 lg:mr-3rem sm:mx-1 mt-1" src={blobs} muted autoPlay loop></video>
-                     }
+                     } */}
+                     
+                     <div className="embed w-full h-full border-round-2xl">
+                        <iframe src="https://player.vimeo.com/video/1016808950?background=1&amp;muted=1&amp;badge=0&amp;loop=1&amp;autopause=0&amp;controls=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" title="smoke" className='w-full h-full border-round-3xl'>
+                        </iframe>
+                    </div>
                         <div className='btns flex justify-content-end align-items-end m-3'> 
                             <Button label="More about me.." type="button" className="mr-3 p-button-raised" />
                         </div>
